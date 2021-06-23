239f8521fb4fc94583e52f807454c2f8e8cd9c2f
Takumi-Koyama <t-koyama@ediplex.com>
2021-06-23 17:17:45 +0900

    ルーティングを作成

---
+++ backend/app/Http/Controllers/BoxController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Box;
+use Illuminate\Http\Request;
+
+class BoxController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function get()
+    {
+        return "a";
+    }
+}
